3b1752ca4771a9fcb6d35b3d448a7d07cd634ffe
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-02-05 18:58:11 +0530

    Create tempData.js

---
+++ tempData.js
@@ -0,0 +1,66 @@
+export default tempData=[
+  { 
+    id:1,
+    name:'Plan a Trip',
+    color:"#24A6D9",
+    todos:[
+      {
+        title:"Book 📙 Flight",
+        completed:false
+      },
+      {
+        title:"Passport 🛂 Check",
+        completed:true
+      },{
+        title:"Reserve Hotel 🏩 Room",
+        completed:true
+      },{
+        title:"Pack Luggage",
+        completed:false
+      },
+
+    ]
+  },
+   {
+    id:2,
+    name:'Errands',
+    color:"#8022D9",
+    todos:[
+      {
+        title:"Buy Milk 🥛",
+        completed:false
+      },
+      {
+        title:"Plan weekend camping trip",
+        completed:true
+      },{
+        title:"Make a Video 📹",
+        completed:true
+      },{
+        title:"Take dog for a walk",
+        completed:true
+      },
+     
+    ]
+  },
+   {
+     id:3,
+    name:'Birthday Party',
+    color:"#595BD9",
+    todos:[
+      {
+        title:"Get Balloons 🎈",
+        completed:false
+      },
+      {
+        title:"Send Invitations",
+        completed:true
+      },{
+        title:"Mark Dinner Reservations",
+        completed:true
+      },
+    
+    ]
+  }
+
+]
